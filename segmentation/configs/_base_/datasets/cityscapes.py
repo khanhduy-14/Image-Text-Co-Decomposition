5996b8d659c53ae76c3139ec7ce0f2f540e721c5
khanhduy-14 <tranhakhanhduy.dev@gmail.com>
2026-03-17 22:40:41 +0700

    chore: change data root cityscapes

--- segmentation/configs/_base_/datasets/cityscapes.py
+++ segmentation/configs/_base_/datasets/cityscapes.py
@@ -8,8 +8,10 @@
 _base_ = ["../custom_import.py"]
 # dataset settings
 dataset_type = "CityscapesDataset"
-data_root = "./data/cityscapes"
-img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+data_root = "/kaggle/input/datasets/duytranhakhanh/cityscapes-converted"
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True
+)
 test_pipeline = [
     dict(type="LoadImageFromFile"),
     dict(
